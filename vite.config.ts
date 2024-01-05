5ae2d2acb28ed07bc32e21684378b3074b27dee4
xyxc0673 <xyxc0673@gmail.com>
2024-01-05 22:33:24 +0800

    chore: support pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'development',
+  registerType: 'prompt',
+  devOptions: {
+    enabled: true,
+  },
+  includeAssets: ['favicon.svg'],
+  manifest: {
+    name: 'Calendar Remark',
+    short_name: 'Calendar Remark',
+    theme_color: '#ffffff',
+    description: 'A small and clean calendar app',
+    icons: [
+      {
+        src: '/pwa-64x64.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon-180x180.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+      {
+        src: '/maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(pwaOptions)],
+});
