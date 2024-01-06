f0c0bc35cd399b3fb9b593e8f1d864effc9671f4
xyxc0673 <xyxc0673@gmail.com>
2024-01-06 11:55:13 +0800

    refactor: add Dropdown

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -1,84 +1,19 @@
-import { useEffect, useRef, useState } from 'react';
 import { ChevronDown } from '../assets/icons';
 import clsxm from '../libs/clsxm';
 import useCalendar from '../hooks/useCalendar';
 import { useSelectedDate } from '../hooks/useSelectedDate';
 import { isAfterDate, isSameDate } from '../libs/date';
-
-const DateSelector = ({
-  valueList,
-  value,
-  render,
-  onChange,
-}: {
-  valueList: number[];
-  value: number;
-  render: (value: number) => string;
-  onChange: (value: number) => void;
-}) => {
-  const [active, setActive] = useState(false);
-  const listRef = useRef<HTMLDivElement | null>(null);
-  const refs = useRef(new Map<number, HTMLSpanElement | null>());
-
-  useEffect(() => {
-    const currentRef = refs.current.get(value);
-    if (currentRef && listRef.current) {
-      listRef.current.scrollTo({
-        top: currentRef.offsetTop - 4,
-        behavior: 'smooth',
-      });
-    }
-  }, []);
-
-  const handleValueChange = (value: number) => {
-    onChange(value);
-    setActive(false);
-  };
-
-  return (
-    <div className='relative' onMouseLeave={() => setActive(false)}>
-      <div
-        className='flex items-center md:py-1 py-0.5 pl-3 pr-0.5 md:pr-1 transition-colors duration-200 bg-white border border-transparent rounded cursor-pointer hover:border-gray-600'
-        onClick={() => setActive(!active)}
-        onMouseEnter={() => setActive(true)}
-      >
-        <span className='text-sm md:text-base'>{render(value)}</span>
-        <ChevronDown
-          className={clsxm(
-            'w-6 h-6 ml-1 text-gray-500 transition-transform duration-200rotate-180',
-            active && 'rotate-180'
-          )}
-        />
-      </div>
-      <div className='absolute w-full h-1 opacity-0 top-full'></div>
-      <div
-        ref={listRef}
-        className={clsxm(
-          'absolute z-50 flex flex-col invisible w-full translate-y-0 gap-1 p-1 overflow-hidden overflow-y-scroll transition-all duration-300 bg-white rounded shadow opacity-0 h-60 md:h-96 shadow-slate-200 top-full scrollbar-track-white scrollbar-thumb-slate-300 scrollbar-thin scrollbar-thumb-rounded-full',
-          active && 'translate-y-1 opacity-100 visible'
-        )}
-      >
-        {valueList.map((item) => (
-          <span
-            ref={(el) => refs.current.set(item, el)}
-            key={item}
-            className={clsxm(
-              'text-zinc-500 opacity-80 hover:opacity-100 whitespace-nowrap hover:text-slate-900 inline-block w-full px-3 py-1 text-center border border-transparent rounded cursor-pointer hover:border-gray-600 transition-all duration-200 text-sm md:text-base',
-              item === value && 'bg-slate-200 opacity-100 text-slate-900'
-            )}
-            onClick={() => handleValueChange(item)}
-          >
-            {render(item)}
-          </span>
-        ))}
-      </div>
-    </div>
-  );
-};
+import Dropdown from './Dropdown';
 
 const CalendarHeader = () => {
-  const yearList = Array.from({ length: 151 }, (_, i) => i + 1900);
-  const monthList = Array.from({ length: 12 }, (_, i) => i + 1);
+  const yearList = Array.from({ length: 151 }, (_, i) => ({
+    value: i + 1900,
+    label: `${i + 1900}年`,
+  }));
+  const monthList = Array.from({ length: 12 }, (_, i) => ({
+    value: i + 1,
+    label: `${i + 1}月`,
+  }));
   const { today, currentYear, currentMonth, setCurrentMonth, setCurrentYear } =
     useCalendar();
   const { selectedDate, setSelectedDate } = useSelectedDate();
@@ -100,18 +35,16 @@ const CalendarHeader = () => {
     currentMonth > today.getMonth();
 
   return (
-    <div className='relative flex items-center justify-center gap-4 px-3 py-2 md:px-6 md:py-4 bg-slate-100'>
-      <DateSelector
-        valueList={yearList}
+    <div className='relative flex items-center justify-center gap-1.5 px-3 py-2 md:gap-4 md:px-6 md:py-4 bg-slate-100'>
+      <Dropdown
+        options={yearList}
         value={currentYear}
-        render={(value) => `${value}年`}
-        onChange={setCurrentYear}
+        onChange={(item) => setCurrentYear(item.value)}
       />
-      <DateSelector
-        valueList={monthList}
+      <Dropdown
+        options={monthList}
         value={currentMonth + 1}
-        render={(value) => `${value}月`}
-        onChange={(value) => setCurrentMonth(value - 1)}
+        onChange={(item) => setCurrentMonth(item.value - 1)}
       />
       <button
         className={clsxm(
@@ -123,8 +56,8 @@ const CalendarHeader = () => {
       >
         <ChevronDown
           className={clsxm(
-            'h-6 w-0 text-gray-500 transition-all duration-300 rotate-0 scale-50',
-            showBackToToday && 'w-6 scale-100',
+            'h-4 md:h-6 w-0 text-gray-500 transition-all duration-300 rotate-0 scale-50',
+            showBackToToday && 'w-4 md:w-6 scale-100',
             isAfterToday && 'rotate-90',
             !isAfterToday && '-rotate-90',
             !showBackToToday && 'rotate-0'
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,95 @@
+import { useState, useRef, useEffect } from 'react';
+import { ChevronDown } from '../assets/icons';
+import clsxm from '../libs/clsxm';
+
+type DefaultOptionType = {
+  label: React.ReactNode;
+  value: string | number | undefined;
+};
+
+type DropdownProps<T extends DefaultOptionType> = {
+  options: T[];
+  value?: T['value'];
+  className?: string;
+  placeholder?: string;
+  onChange: (item: T) => void;
+};
+
+const Dropdown = <T extends DefaultOptionType>({
+  options,
+  value,
+  className,
+  placeholder,
+  onChange,
+}: DropdownProps<T>) => {
+  const [active, setActive] = useState(false);
+  const listRef = useRef<HTMLDivElement | null>(null);
+  const refs = useRef(new Map<T, HTMLSpanElement | null>());
+  const currentItem = options.find((item) => item.value === value);
+
+  useEffect(() => {
+    if (!currentItem) {
+      return;
+    }
+    const currentRef = refs.current.get(currentItem);
+    if (currentRef && listRef.current) {
+      listRef.current.scrollTo({
+        top: currentRef.offsetTop - 4,
+        behavior: 'smooth',
+      });
+    }
+  }, []);
+
+  const handleChange = (value: T) => {
+    onChange(value);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className={clsxm('relative', className)}
+      onMouseLeave={() => setActive(false)}
+    >
+      <div
+        className='flex items-center md:py-1 py-0.5 pl-1.5 pr-0.5 md:pl-3 md:pr-1 transition-colors duration-200 bg-white border border-transparent rounded cursor-pointer hover:border-gray-600'
+        onClick={() => setActive(!active)}
+        onMouseEnter={() => setActive(true)}
+      >
+        <span className='text-sm md:text-base'>
+          {currentItem ? currentItem.label : placeholder}
+        </span>
+        <ChevronDown
+          className={clsxm(
+            'w-4 h-4 md:w-6 md:h-6 ml-1 text-gray-500 transition-transform duration-200rotate-180',
+            active && 'rotate-180'
+          )}
+        />
+      </div>
+      <div className='absolute w-full h-1 opacity-0 top-full'></div>
+      <div
+        ref={listRef}
+        className={clsxm(
+          'absolute z-50 left-1/2 -translate-x-1/2 flex flex-col invisible w-fit translate-y-0 gap-1 p-1 overflow-hidden overflow-y-auto transition-all duration-300 bg-white rounded shadow opacity-0 max-h-60 md:max-h-96 shadow-slate-200 top-full scrollbar-track-white scrollbar-thumb-slate-300 scrollbar-thin scrollbar-thumb-rounded-full',
+          active && 'translate-y-1 opacity-100 visible'
+        )}
+      >
+        {options.map((option) => (
+          <span
+            ref={(el) => refs.current.set(option, el)}
+            key={option.value}
+            className={clsxm(
+              'text-zinc-500 opacity-80 hover:opacity-100 whitespace-nowrap hover:text-slate-900 inline-block w-full px-3 py-1 text-center border border-transparent rounded cursor-pointer hover:border-gray-600 transition-all duration-200 text-sm md:text-base',
+              option.value === value &&
+                'bg-slate-200 opacity-100 text-slate-900'
+            )}
+            onClick={() => handleChange(option)}
+          >
+            {option?.label}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
