7f02d539891372ffd4597350394ba7db56f87b87
xyxc0673 <xyxc0673@gmail.com>
2024-01-09 21:20:22 +0800

    fix: unexpectedly style

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <>
       <ShareModal />
-      <div className='flex items-center justify-center h-screen p-4 md:p-20 bg-slate-200'>
-        <div className='flex flex-col gap-2'>
+      <div className='flex items-center justify-center w-full h-screen p-4 md:p-20 bg-slate-200'>
+        <div className='flex flex-col gap-2 max-md:w-full'>
           <div className='flex flex-col w-full gap-2 overflow-hidden bg-white rounded-lg md:w-fit h-fit md:flex-row'>
             <div className='w-full md:w-[37.5rem] overflow-hidden bg-white rounded-lg shadow-lg shadow-slate-200'>
               <CalendarHeader />
