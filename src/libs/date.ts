b1f7f3359b744bc27a0805b68bc7ea9dd34c7088
xyxc0673 <xyxc0673@gmail.com>
2024-01-13 10:43:28 +0800

    feat: year progress

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -15,3 +15,15 @@ export const isAfterDate = (date1?: Date, date2?: Date) => {
   }
   return date1.getTime() > date2.getTime();
 };
+
+export const getPercentageOfYear = (date: Date): number => {
+  const startOfYear = new Date(date.getFullYear(), 0, 1); // 当年的第一天
+  const endOfYear = new Date(date.getFullYear(), 11, 31); // 当年的最后一天
+
+  const totalMilliseconds = endOfYear.getTime() - startOfYear.getTime(); // 当年的总毫秒数
+  const elapsedMilliseconds = date.getTime() - startOfYear.getTime(); // 已过去的毫秒数
+
+  const percentage = (elapsedMilliseconds / totalMilliseconds) * 100; // 计算百分比
+
+  return Math.round(percentage * 100) / 100; // 返回百分比，保留两位小数
+};
