455cc5432406c8be395fb8da4dbd9811b68e12b5
xyxc0673 <xyxc0673@gmail.com>
2024-01-13 15:37:06 +0800

    refactor: extend clickable area

--- src/components/RecentHoliday.tsx
+++ src/components/RecentHoliday.tsx
@@ -27,10 +27,11 @@ const RecentHoliday = () => {
     <div className='flex items-center justify-between px-3 py-2 bg-slate-100 md:px-6 md:py-4 dark:bg-zinc-900/20'>
       <div className='flex items-center w-full text-xs text-gray-700 md:text-sm dark:text-zinc-200'>
         距离
-        <div className='h-6 flex items-center mx-1 transition-all duration-200 rounded cursor-pointer hover:pr-0 group hover:bg-white dark:hover:bg-zinc-700 px-1 py-0.5 border border-transparent hover:border-gray-600'>
-          <span className='font-bold' onClick={navigateToHoliday}>
-            {details.chinese}
-          </span>
+        <div
+          className='h-6 flex items-center mx-1 transition-all duration-200 rounded cursor-pointer hover:pr-0 group hover:bg-white dark:hover:bg-zinc-700 px-1 py-0.5 border border-transparent hover:border-gray-600'
+          onClick={navigateToHoliday}
+        >
+          <span className='font-bold'>{details.chinese}</span>
           <ChevronDown className='w-0 h-6 transition-all duration-200 -rotate-90 dark:stroke-zinc-200 stroke-zinc-800 group-hover:w-6' />
         </div>
         还有
