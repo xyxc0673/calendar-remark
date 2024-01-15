5a2acd7460c8bf50822c523339ce57fe7f21f68c
xyxc0673 <xyxc0673@gmail.com>
2024-01-15 20:14:30 +0800

    feat: custom sharing image

--- src/components/ShareModal.tsx
+++ src/components/ShareModal.tsx
@@ -3,14 +3,23 @@ import clsxm from '../libs/clsxm';
 import Calendar from './Calendar';
 import { useToPng } from '@hugocxl/react-to-image';
 import { downloadFromBase64 } from '../libs/download';
+import Checkbox from './Checkbox';
+import { useState } from 'react';
+import { ChevronDown } from '../assets/icons';
+import Divider from './Divider';
 
 export const shareModalOpenAtom = atom<boolean>(false);
 
 const ShareModal = () => {
   const [isOpen, setIsOpen] = useAtom(shareModalOpenAtom);
+  const [showHeader, setShowHeader] = useState(true);
+  const [showFooter, setShowFooter] = useState(true);
+  const [headerText, setHeaderText] = useState('节假日安排');
+  const [footerText, setFooterText] = useState('Calendar Remark');
+  const [showCustomArea, setShowCustomArea] = useState(false);
   const [state, covertToPng, ref] = useToPng<HTMLDivElement>({
     onSuccess: (data) => {
-      downloadFromBase64(data, '节假日 - Calendar Remark.png');
+      downloadFromBase64(data, `${headerText} - Calendar Remark.png`);
     },
   });
 
@@ -20,10 +29,86 @@ const ShareModal = () => {
     }
   };
 
+  const renderCustomArea = () => {
+    return (
+      <div
+        className={clsxm(
+          'relative px-1 mt-2 mx-2 overflow-hidden transition-all duration-200 rounded-lg p-2 text-sm md:text-base',
+          showCustomArea && 'bg-slate-100'
+        )}
+      >
+        <div
+          className='flex items-center cursor-pointer'
+          onClick={() => setShowCustomArea(!showCustomArea)}
+        >
+          <ChevronDown
+            className={clsxm(
+              'size-6 transition-all duration-200',
+              showCustomArea && 'rotate-180'
+            )}
+          />
+          <div>定制化</div>
+        </div>
+        <div
+          className={clsxm(
+            'transition-all duration-200 flex flex-col justify-center',
+            showCustomArea
+              ? 'h-40 opacity-100 visible'
+              : 'h-0 opacity-0 invisible'
+          )}
+        >
+          <Divider direction='horizontal' className='my-2' />
+          <div className='flex flex-col gap-3'>
+            <div className='flex flex-col gap-1'>
+              <div className='flex items-center gap-2'>
+                <span className='text-nowrap'>显示头部</span>
+                <Checkbox
+                  checked={showHeader}
+                  onChange={() => setShowHeader(!showHeader)}
+                />
+              </div>
+              <div className='flex items-center justify-center w-full mt-2'>
+                <input
+                  type='text'
+                  placeholder='例如：节假日安排'
+                  maxLength={20}
+                  value={headerText}
+                  className='w-full px-2 py-1 text-sm transition-all duration-200 border border-gray-300 rounded-md dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:border-transparent'
+                  onChange={(e) => setHeaderText(e.target.value)}
+                  disabled={!showHeader}
+                />
+              </div>
+            </div>
+            <div className='flex flex-col gap-1'>
+              <div className='flex items-center gap-2'>
+                <span className='text-nowrap'>显示底部</span>
+                <Checkbox
+                  checked={showFooter}
+                  onChange={() => setShowFooter(!showFooter)}
+                />
+              </div>
+              <div className='flex items-center justify-center w-full mt-2'>
+                <input
+                  type='text'
+                  placeholder='例如：Calendar Remark'
+                  maxLength={20}
+                  value={footerText}
+                  className='w-full px-2 py-1 text-sm transition-all duration-200 border border-gray-300 rounded-md dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:border-transparent'
+                  onChange={(e) => setFooterText(e.target.value)}
+                  disabled={!showFooter}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div
       className={clsxm(
-        'fixed inset-0 z-10 flex justify-center items-center transition-all duration-300 invisible',
+        'fixed inset-0 z-20 flex justify-center items-center transition-all duration-300 invisible',
         isOpen ? 'visible bg-black/40' : 'invisible'
       )}
     >
@@ -38,17 +123,25 @@ const ShareModal = () => {
             <div>
               <div ref={ref} className='p-2 bg-white dark:bg-zinc-600'>
                 <div className='bg-white dark:bg-zinc-800 w-full md:w-[37.5rem] rounded-lg overflow-hidden  md:shadow-lg shadow-slate-200 text-sm md:text-base'>
-                  <div className='w-full px-1 py-2 text-center md:px-2 md:py-4 bg-slate-100 dark:bg-zinc-900/20 dark:text-zinc-200'>
-                    Calendar Remark
-                  </div>
+                  {showHeader && (
+                    <div className='w-full px-1 py-2 text-center md:px-2 md:py-4 bg-slate-100 dark:bg-zinc-900/20 dark:text-zinc-200'>
+                      {headerText}
+                    </div>
+                  )}
                   <Calendar isSharing />
-                  <div className='flex items-center justify-center w-full gap-1 px-1 py-2 text-sm md:gap-2 md:px-2 md:py-4 bg-slate-100 dark:bg-zinc-900/20 md:text-base dark:text-zinc-200'>
-                    <img src='/favicon.svg' className='w-4 h-4 md:w-6 md:h-6' />
-                    <span>calendar.xym.im</span>
-                  </div>
+                  {showFooter && (
+                    <div className='flex items-center justify-center w-full gap-1 px-1 py-2 text-sm md:gap-2 md:px-2 md:py-4 bg-slate-100 dark:bg-zinc-900/20 md:text-base dark:text-zinc-200'>
+                      <img
+                        src='/favicon.svg'
+                        className='w-4 h-4 md:w-6 md:h-6'
+                      />
+                      <span>{footerText}</span>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
+            {renderCustomArea()}
           </div>
           <div className='w-full h-px bg-gray-200'></div>
           <div className='flex justify-end gap-4 px-6 py-4'>
