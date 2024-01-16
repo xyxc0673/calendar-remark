d2662a15f3a853ca61666f1cbab8ad93471f74c2
xyxc0673 <xyxc0673@gmail.com>
2024-01-16 19:52:57 +0800

    fix: view overflows on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
       <ShareModal />
-      <div className='flex items-center justify-center w-full h-screen p-4 md:p-20 bg-slate-200 dark:bg-black/80'>
+      <div className='flex items-center justify-center w-full min-h-screen p-4 md:p-20 bg-slate-200 dark:bg-black/80'>
         <div className='flex flex-col gap-3 max-md:w-full'>
           <div className='flex flex-col w-full gap-2 overflow-hidden bg-white rounded-lg dark:bg-zinc-800 md:w-fit h-fit md:flex-row'>
             <div className='w-full md:w-[37.5rem] overflow-hidden rounded-lg shadow-lg md:border-r max-md:dark:border-b shadow-slate-200 dark:shadow-none border-zinc-400/20'>
