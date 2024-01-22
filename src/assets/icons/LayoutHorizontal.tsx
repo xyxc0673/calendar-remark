2278a8f4fb8a0a98f073d2ef562557635a76aa31
xyxc0673 <xyxc0673@gmail.com>
2024-01-22 20:53:43 +0800

    style: unify stroke color

--- src/assets/icons/LayoutHorizontal.tsx
+++ src/assets/icons/LayoutHorizontal.tsx
@@ -8,7 +8,7 @@ export function LayoutHorizontal(props: React.SVGProps<SVGSVGElement>) {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 24 24'
-      stroke='#111'
+      stroke='#475569'
       {...props}
     >
       <path
